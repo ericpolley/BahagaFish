cd32c929f8d72b71dc0c159820f6168ae27ed54f
eric polley <ericpolley@live.com>
2022-12-27 14:53:49 -0800

    center menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ export default function Header() {
   return (
 
     <div className='flex justify-center items-center shadow-xl border-2 border-gray-300 bg-blue-100'>
-            <div className='w-[90%] md:w-[77%] flex justify-between'>
+            <div className='w-[90%] md:w-[77%] flex justify-between items-center'>
         
         <div className='flex md:flex-row flex-col justify-center items-center w-full'> 
         <img src="https://cdn.pixabay.com/photo/2021/01/09/22/55/pig-5903673_960_720.png" onClick={()=>navigate('/')} alt="pig" className='cursor-pointer h-16 p-1 bg-blue-100 md:m-3 border-blue-200 border-4 rounded-full' />
